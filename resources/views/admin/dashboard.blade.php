5c491db23e916ccdb8e7a2836bc0b72e08487a05
YemsAvilaGalvez <avilagalvezraul@gmail.com>
2026-03-18 15:43:37 -0500

    configuracion inicial del proyecto

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,15 @@
+<x-admin-layout 
+title="Dashboard | inventario"
+:breadcrumbs="[
+    [
+        'name' => 'Dashboard', 
+        'href' => route('admin.dashboard')
+    ],
+    [
+        'name' => 'Home', 
+    ]
+]">
+    <x-wire-button>
+        prueba
+    </x-wire-button>
+</x-admin-layout>
